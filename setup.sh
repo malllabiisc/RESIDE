01c71c0470c1036575b9b9ef934684d160e240cd
rishabhjoshi <rj04796@live.com>
2018-08-27 21:41:04 +0530

    Glove embeddings

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 wget http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip -d glove
 python -m gensim.scripts.glove2word2vec -i glove/glove.6B.300d.txt -o glove/glove.6B.300d_word2vec.txt
+python -m gensim.scripts.glove2word2vec -i glove/glove.6B.100d.txt -o glove/glove.6B.100d_word2vec.txt
+python -m gensim.scripts.glove2word2vec -i glove/glove.6B.200d.txt -o glove/glove.6B.200d_word2vec.txt
+python -m gensim.scripts.glove2word2vec -i glove/glove.6B.50d.txt -o glove/glove.6B.50d_word2vec.txt
 rm glove.6B.zip
 
 # Create log directory
-mkdir log
+#mkdir log
